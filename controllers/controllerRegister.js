95d2a8d46aa332fea01bfb11beb161d26ed32977
Miguel <miguelsilvapeloso@gmail.com>
2025-05-28 15:53:02 -0300

    add: README.md

--- controllers/controllerRegister.js
+++ controllers/controllerRegister.js
@@ -1,44 +1,47 @@
-const db = require('../models'); 
-const bcrypt = require('bcrypt');
-const validatorForm = require('../utils/validatorForm');
-const speakeasy = require('speakeasy');
-const QRCode = require('qrcode');
+const db = require("../models");
+const bcrypt = require("bcrypt");
+const validatorForm = require("../utils/validatorForm");
+const speakeasy = require("speakeasy");
+const QRCode = require("qrcode");
 
 const getRegister = async (req, res) => {
-    res.render('register', { layout: 'noMenu.handlebars' });
-}
-
-const registerUser = async (req, res) => {
-    const hasError = await validatorForm.registerFormValidator(req, res);
-
-    if (hasError) return;
-
-    const { login, senha } = req.body;
-    
-  try {
-        const secretUser = speakeasy.generateSecret({ length: 20, name: 'Sistema de Configuração Segura entre Usuários (Alice)' });
-        const qrCodeDataURL = await QRCode.toDataURL(secretUser.otpauth_url);
-        const hash = await bcrypt.hash(senha, 10);
-
-        await db.Usuario.create({ login, senha: hash, twoFactorSecret: secretUser.base32 });
-        
-        res.render('register', { 
-            sucesso: 'Usuário cadastrado com sucesso!' ,
-            qrCode: qrCodeDataURL,
-            chaveManual: secretUser.base32,
-            layout: 'noMenu.handlebars'
-        });
-  
-    } catch (error) {
-        console.error(error); 
-        res.render('cadastro', { erro: 'Erro ao criar usuário.' });
-  }
-}
-
-
-module.exports = {
-    getRegister,
-    registerUser
+	res.render("register", { layout: "noMenu.handlebars" });
 };
 
-  
+const registerUser = async (req, res) => {
+	const hasError = await validatorForm.registerFormValidator(req, res);
+
+	if (hasError) return;
+
+	const { login, senha } = req.body;
+
+	try {
+		const secretUser = speakeasy.generateSecret({
+			length: 20,
+			name: "Sistema de Configuração Segura entre Usuários",
+		});
+		const qrCodeDataURL = await QRCode.toDataURL(secretUser.otpauth_url);
+		const hash = await bcrypt.hash(senha, 10);
+
+		await db.Usuario.create({
+			login,
+			senha: hash,
+			twoFactorSecret: secretUser.base32,
+		});
+
+		res.render("register", {
+			sucesso: "Usuário cadastrado com sucesso!",
+			qrCode: qrCodeDataURL,
+			chaveManual: secretUser.base32,
+			layout: "noMenu.handlebars",
+		});
+	} catch (error) {
+		console.error(error);
+		res.render("cadastro", { erro: "Erro ao criar usuário." });
+	}
+};
+
+module.exports = {
+	getRegister,
+	registerUser,
+};
--- utils/validatorForm.js
+++ utils/validatorForm.js
@@ -1,42 +1,49 @@
-const db = require('../models');
+const db = require("../models");
 
 const EMAIL_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 const PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).{8,}$/;
 
 const registerFormValidator = async (req, res) => {
-  const { login, senha, confirmarSenha } = req.body;
+	const { login, senha, confirmarSenha } = req.body;
 
-  let error = null;
+	let error = null;
 
-  if (!login) {
-    error = { erro: 'Necessário informar um login', campo: 'login' };
-  } else if (!EMAIL_REGEX.test(login)) {
-    error = { erro: 'E-mail inválido. Use o formato usuario@dominio.com', campo: 'login' };
-  } else if (!senha) {
-    error = { erro: 'Necessário informar uma senha', campo: 'senha' };
-  } else if (!PASSWORD_REGEX.test(senha)) {
-    error = { erro: 'Senha inválida. Use pelo menos 8 caracteres, incluindo letras maiúsculas, minúsculas, números e símbolos', campo: 'senha' };
-  } else if (!confirmarSenha) {
-    error = { erro: 'Necessário confirmar a senha', campo: 'confirmarSenha' };
-  } else if (senha !== confirmarSenha) {
-    error = { erro: 'As senhas não coincidem, tente novamente!', campo: 'confirmarSenha' };
-  }
+	if (!login) {
+		error = { erro: "Necessário informar um login", campo: "login" };
+	} else if (!senha) {
+		error = { erro: "Necessário informar uma senha", campo: "senha" };
+	} else if (!PASSWORD_REGEX.test(senha)) {
+		error = {
+			erro: "Senha inválida. Use pelo menos 8 caracteres, incluindo letras maiúsculas, minúsculas, números e símbolos",
+			campo: "senha",
+		};
+	} else if (!confirmarSenha) {
+		error = { erro: "Necessário confirmar a senha", campo: "confirmarSenha" };
+	} else if (senha !== confirmarSenha) {
+		error = {
+			erro: "As senhas não coincidem, tente novamente!",
+			campo: "confirmarSenha",
+		};
+	}
 
-  if (!error) {
-    const existsUser = await db.Usuario.findOne({ where: { login } });
-    if (existsUser) {
-      error = { erro: 'Já existe um usuário com o mesmo login', campo: 'login' };
-    }
-  }
+	if (!error) {
+		const existsUser = await db.Usuario.findOne({ where: { login } });
+		if (existsUser) {
+			error = {
+				erro: "Já existe um usuário com o mesmo login",
+				campo: "login",
+			};
+		}
+	}
 
-  if (error) {
-    await res.render('register', { error, layout: 'noMenu.handlebars' });
-    return true;
-  }
+	if (error) {
+		await res.render("register", { error, layout: "noMenu.handlebars" });
+		return true;
+	}
 
-  return false;
+	return false;
 };
 
 module.exports = {
-    registerFormValidator
-}
+	registerFormValidator,
+};
